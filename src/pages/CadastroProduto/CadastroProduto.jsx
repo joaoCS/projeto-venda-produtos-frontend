5b8514caed94dfdfc561b555eb6e0387a7c2d177
joaoCS <joaoantoniogba@hotmail.com>
2023-03-16 17:47:04 -0300

    Adicionada venda de produtos para clientes

--- src/pages/CadastroProduto/CadastroProduto.jsx
+++ src/pages/CadastroProduto/CadastroProduto.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useCookies } from 'react-cookie';
 
 export const CadastroProduto = () => {
+
+    const [categorias, setCategorias] = useState([]);
+    
+    const [cookies, setCookies] = useCookies(["access_token"]);
 
     const [productData, setProductData] = useState({
         nome: "",
@@ -9,40 +14,53 @@ export const CadastroProduto = () => {
         valorVenda: 0.0,
         categoria: ""
     });
-
+    
     function handleChange(event) {
         const {name, value} = event.target;
 
         if (name=="valorCompra" || name=="valorVenda") 
-            setProductData({...productData, [name]: parseFloat(value)});
+        setProductData({...productData, [name]: parseFloat(value)});
 
         else
             setProductData({...productData, [name]: value});
     }
-
+    
     async function submit(event){
         event.preventDefault();
+
         try{
 
             console.log(productData);
-
-            let url = 'http://localhost:3001/cadastrarProduto/';
             
-            const response = await axios.post(url, {
-                    nome: productData.nome,
-                    valorCompra: productData.valorCompra,
-                    valorVenda: productData.valorVenda,
-                    categoria: productData.categoria
-            });
+            let url = 'http://localhost:3001/produtos/create';
+            
+            const response = await axios.post(url, productData, { headers: { authorization: cookies.access_token } });
         }
         catch(err){
             console.log(err);
-
-
         }
     }
+    
+    useEffect(()=>{
 
+        async function fetchCategorias() {
+            try {
+                const response = await axios.get('http://localhost:3001/categorias/');
+                
+                console.log(response.data);
 
+                setCategorias(response.data);
+
+            } 
+            catch (err) {
+                console.log(err);
+            }
+        }
+
+        fetchCategorias();
+
+    }, []);
+    
     return (
         <div>
             <h2>Cadastrar Produto</h2>
@@ -61,7 +79,14 @@ export const CadastroProduto = () => {
                 </div>
                 <div>
                     <label htmlFor="categoria">Categoria:</label>
-                    <select name="categoria" id="categoria" onChange={handleChange}>Selecionar categoria:</select>
+                    <select name="categoria" id="categoria" onChange={handleChange}>
+                       <option>Selecionar categoria:</option>
+                       {categorias.map((categoria, idx) => {
+                            return (
+                                <option key={idx} value={categoria._id}>{categoria.nome}</option>
+                            );
+                       })}
+                    </select>
                 </div>
                 <button type="submit">Cadastrar</button>
             </form>
--- src/pages/Venda/Venda.jsx
+++ src/pages/Venda/Venda.jsx
@@ -1,44 +1,180 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import { useCookies } from 'react-cookie';
 
 export const Venda = () => {
     
-    const [ produtos, setProdutos ] = useState([]);
-   
-    async function buscaProdutos(nome) {
+    const [ produtos, setProdutos ] = useState(()=>[]);
+    const [ venda, setVenda ] = useState([]);
+    const [ clientes, setClientes ]  = useState([]);
+    const [ vendaData, setVendaData] = useState({ cliente: { }, venda, totalPagar: 0 });
+
+    let vendas = [];
+
+    const [cookies, setCookies] = useCookies(["access_token"]);
+
+    async function buscaProdutos() {
         try{
             
-            const response = await axios.get('http://localhost:3000/buscaProdutos', { nome });
+            const response = await axios.get('http://localhost:3001/produtos', {headers:{
+                authorization: cookies.access_token
+            }});
             
             setProdutos(response.data);
-            console.log(produtos);
-
         }
         catch(err) {
             console.log(err);
         }
     }
 
+    function atualizaTotalPagar(){
+        let total = 0 ;
+
+        for (let index = 0; index < vendas.length; index++) {
+            total += vendas[index].quantidade * vendas[index].valorVenda;
+            
+        }
+
+        setVendaData({ ...vendaData, totalPagar: total });
+    }
+
+    function adicionaProduto(event) {
+        event.preventDefault();
+
+        const index = event.target.index.value;
+
+        let v = [...venda];
+
+        let prod = produtos;
+
+        prod[index].quantidade = 1;
+        v.push(prod[index]);
+        
+        
+        setVenda([...v]);
+        vendas = [...v];
+        
+        prod.splice(index, 1);
+
+        setProdutos([...prod]);
+
+        atualizaTotalPagar();
+      
+        console.log(vendas);
+
+    }
+    
+    function removeProduto(event) {
+        event.preventDefault();
+
+        const index = event.target.name;
+        
+        let prod = [...produtos];
+        let v = [...venda];
+
+        prod.push(v[index]);
+        setProdutos(prod);
+
+        v.splice(index, 1);
+        setVenda(v);
+
+        vendas = v;
+
+        atualizaTotalPagar();
+    }
+
+    async function fetchClientes(){
+                
+        try {
+            const response = await axios.get("http://localhost:3001/clientes/");
+
+            setClientes(response.data);
+        } catch (err) {
+            alert("Erro ao buscar clientes!", err)
+        }
+
+    }
+
+    function selecionaCliente(event){
+        const index = event.target.value;
+
+        vendaData.cliente = clientes[index];
+        vendaData.venda = [...venda];
+
+
+        console.log(vendaData);
+        
+    }
+    function setQtd(event){
+        const quantidade = event.target.value;
+        const index = event.target.name;
+
+        let v = [...venda];
+        v[index].quantidade = quantidade;
+        setVenda(v);
+        vendas = v;
+
+        console.log(venda[index]);
+
+        atualizaTotalPagar();
+    }
+
     useEffect(()=>{
 
-        buscaProdutos("");
-
+        buscaProdutos();
+        fetchClientes();
     }, []);
 
     return (
         <div>
-            <ul>
+            <h2>Escolher produtos</h2>
+            <div>
                 {produtos.map((produto, idx)=>{
-                    return (<li key={idx}>
-                                <strong>Nome:</strong>
-                                <span>{produto.nome}</span>
-                                <strong>Preço:</strong>
-                                <span>{produto.valorVenda}</span>
-                                <strong>Categoria:</strong>
-                                <span>{produto.categoria}</span>   
-                            </li>);
+                    return (<form key={idx} onSubmit={adicionaProduto} method="post">
+                                
+                                <strong>Nome:</strong> &nbsp;
+                                <span>{produto.nome}</span>&nbsp;
+                                <strong>Preço:</strong> &nbsp;
+                                <span>{produto.valorVenda}</span>&nbsp;
+                                <strong>Categoria:</strong> &nbsp;
+                                <span>{produto.categoria.nome}</span>  &nbsp; 
+                                <input type="hidden" id="index" name="index" value={idx} />
+                                <button type="submit">Adicionar</button>
+                            </form>);
                 })}
-            </ul>
+            </div>
+            <hr />
+            <h2>Venda</h2>
+            <div>
+            {venda.map((v, idx)=>{
+                    return (<div key={idx}>
+                                <strong>Nome:</strong> &nbsp;
+                                <span>{v.nome}</span>&nbsp;
+                                <strong>Preço:</strong> &nbsp;
+                                <span>{v.valorVenda}</span>&nbsp;
+                                <strong>Categoria:</strong> &nbsp;
+                                <span>{v.categoria.nome}</span>  &nbsp; 
+                                <input type="hidden" id="index" name="index" value={idx} />
+                                <strong>Quantidade:</strong>
+                                <input type="number" id="quantidade" name={idx} onChange={setQtd} value={v.quantidade}/>
+
+                                <button onClick={removeProduto} name={idx}>Remover</button>
+                            </div>);
+                })}
+            </div>
+                <hr />
+                <h2>Cliente</h2>
+                <form action="" method="post">
+                    <select name="cliente" id="" onChange={selecionaCliente}>
+                        {clientes.map((cliente, idx) => {
+                            return (
+                                <option id={cliente._id} key={idx} value={idx}>{cliente.nome}</option>
+                            );
+                        })}
+                    </select>
+                </form>
+                <h2>Total a pagar: </h2>
+                <span>{vendaData.totalPagar}</span>
         </div>
     );
 }
