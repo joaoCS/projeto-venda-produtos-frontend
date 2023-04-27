90000b6413461ca1f3a5f0e4cfd1b96d3506ca64
joaoCS <joaoantoniogba@hotmail.com>
2023-04-27 10:09:55 -0300

    feat: atualizado o host do backend

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const api = axios.create({
-    // baseURL: "https://projeto-venda-produtos-back-end-production.up.railway.app/"
-    baseURL: "http://localhost:3001/"
+    baseURL: "https://projeto-venda-produtos-back-end-production.up.railway.app/"
+   // baseURL: "http://localhost:3001/"
 });
 
 export default api;
